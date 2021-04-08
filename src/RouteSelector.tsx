19dffe519b147a3ee9ca5a6fe12dd75509fa9acf
Andi Pätzold <github@andipaetzold.com>
2021-04-08 19:23:32 +0200

    feat: use radio buttons in RouteSelector

--- src/RouteSelector.tsx
+++ src/RouteSelector.tsx
@@ -1,7 +1,8 @@
 import routes from "./routes.json";
 import styles from "./RouteSelector.module.css";
-import c from "classnames";
 import { Route } from "./types";
+import { ChangeEvent } from "react";
+import c from "classnames";
 
 const filteredRoutes: Route[] = ((routes as unknown) as Route[])
   .filter((route) => route.world === "Watopia")
@@ -18,19 +19,31 @@ export default function RouteSelector({
   routeSlug: selectedRouteSlug,
   onChange,
 }: Props) {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      onChange(e.target.value);
+    }
+  };
+
   return (
     <div className={styles.Container}>
       <div className={styles.Sidebar}>
         {filteredRoutes.map((route) => (
-          <button
+          <label
             key={route.slug}
             className={c(styles.Item, {
               [styles.selected]: selectedRouteSlug === route.slug,
             })}
-            onClick={() => onChange(route.slug)}
           >
+            <input
+              type="radio"
+              name="routes"
+              value={route.slug}
+              checked={selectedRouteSlug === route.slug}
+              onChange={handleChange}
+            />
             {route.name}
-          </button>
+          </label>
         ))}
       </div>
     </div>
