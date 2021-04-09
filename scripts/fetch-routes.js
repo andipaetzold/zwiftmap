133dee06247c2c7c41a4c4ff2175668d7cc07b37
Andi Pätzold <github@andipaetzold.com>
2021-04-09 19:15:57 +0200

    chore: download segments in parallel

--- scripts/fetch-routes.js
+++ scripts/fetch-routes.js
@@ -3,18 +3,24 @@ const { writeFileSync } = require("fs");
 const routes = require("../src/data/routes.json");
 
 async function main() {
-  for (const route of routes.filter(route => route.stravaSegmentId !== undefined)) {
-    const response = await fetch(
-      `https://www.strava.com/stream/segments/${route.stravaSegmentId}?streams%5B%5D=latlng&streams%5B%5D=distance&streams%5B%5D=altitude`
-    );
-    const stravaData = await response.json();
-
-    console.log(route.name);
-    writeFileSync(
-      `${__dirname}/../public/segments/${route.slug}.json`,
-      JSON.stringify(stravaData)
-    );
-  }
+  await Promise.all(
+    routes
+      .filter((route) => route.stravaSegmentId !== undefined)
+      .map((route) => fetchRoute(route))
+  );
 }
 
 main();
+
+async function fetchRoute(route) {
+  const response = await fetch(
+    `https://www.strava.com/stream/segments/${route.stravaSegmentId}?streams%5B%5D=latlng&streams%5B%5D=distance&streams%5B%5D=altitude`
+  );
+  const stravaData = await response.json();
+
+  writeFileSync(
+    `${__dirname}/../public/segments/${route.slug}.json`,
+    JSON.stringify(stravaData)
+  );
+  console.log(route.name);
+}
