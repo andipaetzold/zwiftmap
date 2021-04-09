6bc0a67d2fcdc77deed56c2050fc108273f03a3c
Andi Pätzold <github@andipaetzold.com>
2021-04-09 21:39:06 +0200

    feat: add convert script for tiffs

---
+++ mapbox/worlds/convert.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+BASEDIR=$(dirname $0)
+
+convert() {
+    FOLDER=$BASEDIR/$1
+
+    gdal_translate -co TILED=TRUE -co COMPRESS=LZW -co BIGTIFF=YES -expand RGB $FOLDER/georeferenced.tif $FOLDER/converted.tif
+
+    rio calc "(asarray (take a 1) (take a 2) (take a 3))" --co compress=lzw --co tiled=true --co blockxsize=256 --co blockysize=256 --name a=$FOLDER/converted.tif --overwrite $FOLDER/result.tif
+    rio edit-info --nodata 0 $FOLDER/result.tif
+}
+
+convert watopia
