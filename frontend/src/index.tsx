475d15987b2e798fa46c5bd172440a3c9f67d43c
Andi Pätzold <github@andipaetzold.com>
2021-05-30 00:36:47 +0200

    feat: add sentry

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,16 @@
-import "./index.scss";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./index.scss";
+
+Sentry.init({
+  enabled: (process.env.REACT_APP_SENTRY_DSN ?? "").length > 0,
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
