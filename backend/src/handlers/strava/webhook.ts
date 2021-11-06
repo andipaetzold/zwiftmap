a4fb8238a18c59f2b8c1961f4011288c57a13610
Andi Pätzold <github@andipaetzold.com>
2021-11-06 03:01:16 +0100

    feat: validate webhook body

--- backend/src/handlers/strava/webhook.ts
+++ backend/src/handlers/strava/webhook.ts
@@ -1,5 +1,27 @@
 import { Request, Response } from "express";
+import { Literal, Number, Record, Union, Unknown } from "runtypes";
 
-export function handleWebhook(_req: Request, res: Response) {
+const WebhookEvent = Record({
+  aspect_type: Union(Literal("create"), Literal("update"), Literal("delete")),
+  event_time: Number,
+  object_id: Number,
+  object_type: Union(Literal("activity"), Literal("athlete")),
+  owner_id: Number,
+  subscription_id: Number,
+  updates: Unknown,
+});
+
+export function handleWebhook(req: Request, res: Response) {
+  const event = req.body;
+  if (!WebhookEvent.guard(event)) {
+    res.sendStatus(400);
+    return;
+  }
+
+  if (event.object_type !== "activity" || event.aspect_type !== "create") {
+    res.sendStatus(204);
+    return;
+  }
+
   res.sendStatus(204);
 }
