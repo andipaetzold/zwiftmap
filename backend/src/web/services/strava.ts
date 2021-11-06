e2ff2bcab81f8748948bfacdefd6f0014df2fe69
Andi Pätzold <github@andipaetzold.com>
2021-11-07 00:39:52 +0100

    feat: add getToken util method

--- backend/src/web/services/strava.ts
+++ backend/src/web/services/strava.ts
@@ -1,5 +1,11 @@
 import axios from "axios";
 import { STRAVA_CLIENT_ID, STRAVA_CLIENT_SECRET } from "../../shared/config";
+import {
+  readStravaToken,
+  removeStravaToken,
+  StravaToken,
+  writeStravaToken,
+} from "../../shared/persistence/stravaToken";
 
 export const stravaUserAPI = axios.create({
   baseURL: "https://www.strava.com/api/v3",
@@ -12,3 +18,42 @@ export const stravaAppAPI = axios.create({
     client_secret: STRAVA_CLIENT_SECRET,
   },
 });
+
+export async function getToken(athleteId: number): Promise<string | undefined> {
+  let stravaToken = await readStravaToken(athleteId);
+  if (!stravaToken) {
+    return;
+  }
+
+  if (stravaToken.expiresAt < Date.now() / 1_000 - 60) {
+    stravaToken = await refreshToken(stravaToken);
+    if (!stravaToken) {
+      return;
+    }
+  }
+
+  return stravaToken.token;
+}
+
+async function refreshToken(
+  stravaToken: StravaToken
+): Promise<StravaToken | undefined> {
+  try {
+    const response = await stravaAppAPI.post("/oauth/token", {
+      grant_type: "refresh_token",
+      refresh_token: stravaToken.refreshToken,
+    });
+
+    await writeStravaToken({
+      athleteId: stravaToken.athleteId,
+      expiresAt: response.data.expires_at,
+      refreshToken: response.data.refresh_token,
+      token: response.data.access_token,
+    });
+
+    return response.data;
+  } catch {
+    await removeStravaToken(stravaToken.athleteId);
+    return undefined;
+  }
+}
