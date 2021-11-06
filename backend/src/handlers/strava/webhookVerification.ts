6f1f462a0cc6fe555144e6c82ad920ee31389b71
Andi Pätzold <github@andipaetzold.com>
2021-11-06 03:20:16 +0100

    Strava Webhooks (#252)
    * feat: subscribe to strava webhooks
    
    * feat: validate webhook body
    
    * fix: typo

---
+++ backend/src/handlers/strava/webhookVerification.ts
@@ -0,0 +1,16 @@
+import { STRAVA_VERIFY_TOKEN } from "../../config";
+import { Request, Response } from "express";
+
+export function handleWebhookVerification(req: Request, res: Response) {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token) {
+    if (mode === "subscribe" && token === STRAVA_VERIFY_TOKEN) {
+      res.json({ "hub.challenge": challenge });
+    } else {
+      res.sendStatus(403);
+    }
+  }
+}
