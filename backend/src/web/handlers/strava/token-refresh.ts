e3d98ec056903ba582ce819d7afb7d8917d571c3
Andi Pätzold <github@andipaetzold.com>
2021-11-07 00:22:06 +0100

    feat: use runtypes to check token refresh body

--- backend/src/web/handlers/strava/token-refresh.ts
+++ backend/src/web/handlers/strava/token-refresh.ts
@@ -1,17 +1,20 @@
 import { Request, Response } from "express";
+import { Record, String } from "runtypes";
 import { stravaAppAPI } from "../../services/strava";
 
-export async function handleStravaTokenRefresh(req: Request, res: Response) {
-  const refreshToken = req.body.refresh_token;
+const Body = Record({
+  refresh_token: String.withConstraint((t) => t.length > 0),
+});
 
-  if (typeof refreshToken !== "string" || refreshToken.length === 0) {
+export async function handleStravaTokenRefresh(req: Request, res: Response) {
+  if (!Body.guard(req.body)) {
     res.sendStatus(400);
     return;
   }
 
   const response = await stravaAppAPI.post("/oauth/token", {
     grant_type: "refresh_token",
-    refresh_token: refreshToken,
+    refresh_token: req.body.refresh_token,
   });
   res.send(response.data);
 }
