f349198a73ababf0f0d8239365c1dafdc75e860b
Andi Pätzold <github@andipaetzold.com>
2021-11-08 00:53:40 +0100

    fix: run all hooks in the worker

--- backend/src/web/handlers/strava/webhook.ts
+++ backend/src/web/handlers/strava/webhook.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import { removeStravaToken } from "../../../shared/persistence/stravaToken";
 import { stravaWebhookEventQueue } from "../../../shared/queue";
 import { WebhookEvent } from "../../../shared/types";
 
@@ -7,24 +6,6 @@ export async function handleWebhook(req: Request, res: Response) {
   const webhookEvent = req.body;
   if (!WebhookEvent.guard(webhookEvent)) {
     res.sendStatus(400);
-    return;
-  }
-
-  if (
-    webhookEvent.object_type === "athlete" &&
-    webhookEvent.aspect_type === "update" &&
-    webhookEvent.updates["authorized"] === false
-  ) {
-    await removeStravaToken(webhookEvent.object_id);
-    res.sendStatus(204);
-    return;
-  }
-
-  if (
-    webhookEvent.object_type !== "activity" ||
-    webhookEvent.aspect_type !== "create"
-  ) {
-    res.sendStatus(204);
     return;
   }
 
