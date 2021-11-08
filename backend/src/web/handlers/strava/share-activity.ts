0c8ba1ef64541ff4b6af1686b473a28a70aea030
Andi Pätzold <github@andipaetzold.com>
2021-11-09 00:38:45 +0100

    fix: only use types of 'strava' lib

--- backend/src/web/handlers/strava/share-activity.ts
+++ backend/src/web/handlers/strava/share-activity.ts
@@ -1,14 +1,11 @@
+import axios, { AxiosResponse } from "axios";
 import { Request, Response } from "express";
 import { Record, String } from "runtypes";
 import short from "short-uuid";
-import { Strava } from "strava";
-import {
-  FRONTEND_URL,
-  STRAVA_CLIENT_ID,
-  STRAVA_CLIENT_SECRET,
-} from "../../../shared/config";
+import { DetailedActivity, StreamSet } from "strava";
+import { FRONTEND_URL } from "../../../shared/config";
 import { writeSharedItem } from "../../../shared/persistence/sharedItem";
-import { readStravaToken } from "../../../shared/persistence/stravaToken";
+import { getToken, stravaUserAPI } from "../../../shared/services/strava";
 import { getWorld, isZwiftActivity } from "../../../shared/util";
 import { Session } from "../../types";
 
@@ -24,8 +21,8 @@ export async function handleShareActivity(req: Request, res: Response) {
     return;
   }
 
-  const stravaToken = await readStravaToken(session.stravaAthleteId);
-  if (!stravaToken) {
+  const accessToken = await getToken(session.stravaAthleteId);
+  if (!accessToken) {
     res.sendStatus(403);
     return;
   }
@@ -37,35 +34,61 @@ export async function handleShareActivity(req: Request, res: Response) {
 
   const body = req.body;
 
-  const client = new Strava({
-    client_id: `${STRAVA_CLIENT_ID}`,
-    client_secret: STRAVA_CLIENT_SECRET,
-    refresh_token: stravaToken.refreshToken,
-  });
-
-  const activity = await client.activities.getActivityById({
-    id: +body.activityId,
-  });
+  let activityResponse: AxiosResponse<DetailedActivity>;
+  try {
+    activityResponse = await stravaUserAPI.get<DetailedActivity>(
+      `/activities/${body.activityId}`,
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }
+    );
+  } catch (e) {
+    if (axios.isAxiosError(e)) {
+      res.sendStatus(e.response?.status ?? 500);
+    } else {
+      res.sendStatus(500);
+    }
+    return;
+  }
+  const activity = activityResponse.data;
 
   if (!isZwiftActivity(activity)) {
     res.sendStatus(404);
     return;
   }
 
-  const activityStreams = await client.streams.getActivityStreams({
-    id: +body.activityId,
-    keys: [
-      "distance",
-      "latlng",
-      "time",
-      "altitude",
-      "watts",
-      "velocity_smooth",
-      "watts",
-      "cadence",
-      "heartrate",
-    ],
-  });
+  let activityStreamsResponse: AxiosResponse<StreamSet>;
+  try {
+    activityStreamsResponse = await stravaUserAPI.get<StreamSet>(
+      `/activities/${body.activityId}/streams`,
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+        params: {
+          keys: [
+            "distance",
+            "latlng",
+            "time",
+            "altitude",
+            "watts",
+            "velocity_smooth",
+            "watts",
+            "cadence",
+            "heartrate",
+          ].join(","),
+          key_by_type: true,
+        },
+      }
+    );
+  } catch (e) {
+    if (axios.isAxiosError(e)) {
+      res.sendStatus(e.response?.status ?? 500);
+    } else {
+      res.sendStatus(500);
+    }
+    return;
+  }
+
+  const activityStreams = activityStreamsResponse.data;
 
   const world = getWorld(activity)!;
 
--- backend/src/worker/handler/activity-create.ts
+++ backend/src/worker/handler/activity-create.ts
@@ -1,33 +1,24 @@
-import { DetailedActivity, Strava } from "strava";
-import { STRAVA_CLIENT_ID, STRAVA_CLIENT_SECRET } from "../../shared/config";
-import { readStravaToken } from "../../shared/persistence/stravaToken";
+import { DetailedActivity } from "strava";
+import { getToken, stravaUserAPI } from "../../shared/services/strava";
 import { WebhookEventType } from "../../shared/types";
 import { isZwiftActivity } from "../../shared/util";
 
-interface Activity {
-  type: string;
-  device_name: string | null;
-  start_latlng: [latitude: number, longitude: number] | null;
-}
-
 export async function handleActivityCreate(webhookEvent: WebhookEventType) {
-  const token = await readStravaToken(webhookEvent.owner_id);
-  if (!token) {
+  const accessToken = await getToken(webhookEvent.owner_id);
+  if (!accessToken) {
     console.log("Missing strava token");
     return;
   }
 
-  const client = new Strava({
-    client_id: STRAVA_CLIENT_ID.toString(),
-    client_secret: STRAVA_CLIENT_SECRET,
-    refresh_token: token.refreshToken,
-  });
-
   let activity: DetailedActivity;
   try {
-    activity = await client.activities.getActivityById({
-      id: webhookEvent.object_id,
-    });
+    const activityResponse = await stravaUserAPI.get<DetailedActivity>(
+      `/activities/${webhookEvent.object_id}`,
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }
+    );
+    activity = activityResponse.data;
   } catch {
     console.log("Error fetching activity");
     return;
