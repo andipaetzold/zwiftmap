a3d675517d0f08ee743805a2cc5e53ca635c964c
Andi Pätzold <github@andipaetzold.com>
2021-11-09 01:40:03 +0100

    feat: verify query params for authorize endpoints

--- backend/src/web/handlers/strava/authorize-callback.ts
+++ backend/src/web/handlers/strava/authorize-callback.ts
@@ -1,27 +1,41 @@
 import { Request, Response } from "express";
+import { Record, String, Union } from "runtypes";
 import { URLSearchParams } from "url";
 import { FRONTEND_URL } from "../../../shared/config";
 import { writeStravaToken } from "../../../shared/persistence/stravaToken";
 import { stravaAppAPI } from "../../../shared/services/strava";
 import { Session } from "../../types";
 
+const SuccessQuery = Record({
+  state: String,
+  code: String,
+  scope: String,
+});
+
+const ErrorQuery = Record({
+  state: String,
+  error: String,
+});
+
+const Query = Union(SuccessQuery, ErrorQuery);
+
 export async function handleStravaAuthorizeCallback(
   req: Request,
   res: Response
 ) {
+  const query = req.query;
+  if (!Query.guard(query)) {
+    res.sendStatus(400);
+    return;
+  }
+
   const params = new URLSearchParams();
 
-  if (!req.query.error) {
+  if (!("error" in query)) {
     const session: Session = req.session;
-    const code = req.query.code;
-
-    if (typeof code !== "string" || code.length === 0) {
-      res.sendStatus(400);
-      return;
-    }
 
     const response = await stravaAppAPI.post("/oauth/token", {
-      code: code,
+      code: query.code,
       grant_type: "authorization_code",
     });
     const responseJSON = response.data;
@@ -39,17 +53,15 @@ export async function handleStravaAuthorizeCallback(
   }
 
   let path = "/";
-  if (typeof req.query.state === "string") {
-    try {
-      const state: { path: string; search: Record<string, string> } =
-        JSON.parse(req.query.state);
+  try {
+    const state: { path: string; search: { [key: string]: string } } =
+      JSON.parse(query.state);
 
-      path = state.path;
-      Object.entries(state.search).forEach(([key, value]) => {
-        params.set(key, value);
-      });
-    } catch {}
-  }
+    path = state.path;
+    Object.entries(state.search).forEach(([key, value]) => {
+      params.set(key, value);
+    });
+  } catch {}
   const paramsString = params.toString();
 
   let redirectUrl = `${FRONTEND_URL}${path}`;
--- backend/src/web/handlers/strava/authorize.ts
+++ backend/src/web/handlers/strava/authorize.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { Record, String } from "runtypes";
 import { URLSearchParams } from "url";
 import {
   BACKEND_URL,
@@ -6,7 +7,16 @@ import {
   STRAVA_CLIENT_ID,
 } from "../../../shared/config";
 
+const Query = Record({
+  state: String,
+});
+
 export function handleStravaAuthorize(req: Request, res: Response) {
+  if (!Query.guard(req.query)) {
+    res.sendStatus(400);
+    return;
+  }
+
   const params = new URLSearchParams();
   params.set("client_id", STRAVA_CLIENT_ID.toString());
   params.set("redirect_uri", `${BACKEND_URL}/strava/callback`);
@@ -18,7 +28,7 @@ export function handleStravaAuthorize(req: Request, res: Response) {
       ? "activity:write,activity:read_all"
       : "activity:read_all"
   );
-  params.set("state", (req.query.state as string) ?? "{}");
+  params.set("state", req.query.state);
   const url = `https://www.strava.com/oauth/authorize?${params.toString()}`;
 
   res.redirect(url);
