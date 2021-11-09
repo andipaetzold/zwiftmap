6e33c7097dc61bff4aeec3aa00fed94c16dea844
Andi Pätzold <github@andipaetzold.com>
2021-11-09 21:25:00 +0100

    feat: improce sentry tracing

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,7 @@ import { Integrations } from "@sentry/tracing";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BACKEND_HOST } from "./config";
 import "./index.scss";
 import { listenForStravaToken } from "./services/listenForStravaToken";
 import { ping } from "./services/ping";
@@ -11,7 +12,21 @@ import { ping } from "./services/ping";
 Sentry.init({
   enabled: (process.env.REACT_APP_SENTRY_DSN ?? "").length > 0,
   dsn: process.env.REACT_APP_SENTRY_DSN,
-  integrations: [new Integrations.BrowserTracing()],
+  integrations: [
+    new Integrations.BrowserTracing({
+      tracingOrigins: [BACKEND_HOST],
+      beforeNavigate: (context) => {
+        const pathParts = context.name.split("/");
+        if (pathParts.length >= 2) {
+          pathParts[1] = ":worldSlug";
+        }
+        return {
+          ...context,
+          name: pathParts.join("/"),
+        };
+      },
+    }),
+  ],
   tracesSampleRate: 1.0,
 });
 
