22d545a20144abb819a6cca9049d30a016c5b053
Andi Pätzold <github@andipaetzold.com>
2021-11-10 13:20:34 +0100

    feat: improve event caching

--- frontend/src/services/events/api.ts
+++ frontend/src/services/events/api.ts
@@ -1,22 +1,30 @@
 import axios from "axios";
 import { ZwiftEvent } from "./types";
 
-let cache: Promise<ZwiftEvent[]>;
+let listCache: Promise<ZwiftEvent[]> | undefined;
 
 const api = axios.create({
   baseURL: "https://us-or-rly101.zwift.com/api",
 });
 
 export async function fetchEvents(): Promise<ZwiftEvent[]> {
-  if (!cache) {
-    cache = api
+  if (!listCache) {
+    listCache = api
       .get<ZwiftEvent[]>("/public/events/upcoming")
       .then((r) => r.data);
   }
 
-  return await cache;
+  return await listCache;
 }
 
 export async function fetchEvent(eventId: string): Promise<ZwiftEvent> {
+  if (listCache) {
+    const allEvents = await listCache;
+    const eventInList = allEvents.find((e) => e.id.toString() === eventId);
+    if (eventInList) {
+      return eventInList;
+    }
+  }
+
   return api.get<ZwiftEvent>(`/public/events/${eventId}`).then((r) => r.data);
 }
