b86e1e80afd96de889ab6e3a6152dd6c1641bc63
Andi Pätzold <github@andipaetzold.com>
2021-11-22 00:47:29 +0100

    fix: do not crash if browser doesn't support IntersectionObserver

--- frontend/src/hooks/useOnScreen.ts
+++ frontend/src/hooks/useOnScreen.ts
@@ -1,24 +1,31 @@
 import { MutableRefObject, useEffect, useState } from "react";
 
+const isSupported = "IntersectionObserver" in window;
+
 /**
  * @see https://usehooks.com/useOnScreen/
  */
 export function useOnScreen(ref: MutableRefObject<HTMLElement | null>) {
   const [isIntersecting, setIntersecting] = useState(false);
   useEffect(() => {
-    const current = ref.current;
+    if (isSupported) {
+      const current = ref.current;
 
-    const observer = new IntersectionObserver(([entry]) => {
-      setIntersecting(entry.isIntersecting);
-    });
-    if (current) {
-      observer.observe(current);
-    }
-    return () => {
+      const observer = new window.IntersectionObserver(([entry]) => {
+        setIntersecting(entry.isIntersecting);
+      });
       if (current) {
-        observer.unobserve(current);
+        observer.observe(current);
       }
-    };
+      return () => {
+        if (current) {
+          observer.unobserve(current);
+        }
+      };
+    } else {
+      // always return true if browser doesn't support IntersectionObserver
+      setIntersecting(true);
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return isIntersecting;
