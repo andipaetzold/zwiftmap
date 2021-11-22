4a6cbbe2011560b8a254b216d54a3cb247a13584
Andi Pätzold <github@andipaetzold.com>
2021-11-22 13:32:45 +0100

    feat: use hashmap for share lookup

--- backend/src/shared/persistence/redis.ts
+++ backend/src/shared/persistence/redis.ts
@@ -8,12 +8,12 @@ export const redisClient = createClient({
 export async function write<T = any>(key: string, value: T): Promise<void> {
   const stringified = JSON.stringify(value);
 
-  await new Promise((resolve, reject) => {
-    redisClient.set(key, stringified, (err, reply) => {
+  await new Promise<void>((resolve, reject) => {
+    redisClient.set(key, stringified, (err) => {
       if (err) {
         reject(err);
       } else {
-        resolve(reply);
+        resolve();
       }
     });
   });
@@ -50,6 +50,39 @@ export async function exists(key: string): Promise<boolean> {
         reject(err);
       } else {
         resolve(reply === 1);
+      }
+    });
+  });
+}
+
+export async function hset<T = any>(
+  key: string,
+  field: string,
+  value: T
+): Promise<void> {
+  const stringified = JSON.stringify(value);
+
+  await new Promise<void>((resolve, reject) => {
+    redisClient.hset(key, field, stringified, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+export async function hget<T = any>(
+  key: string,
+  field: string
+): Promise<T | undefined> {
+  return await new Promise((resolve, reject) => {
+    redisClient.hget(key, field, (err, value) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(value ? JSON.parse(value) : undefined);
       }
     });
   });
--- backend/src/shared/persistence/share.ts
+++ backend/src/shared/persistence/share.ts
@@ -3,7 +3,10 @@ import objectHash from "object-hash";
 import short from "short-uuid";
 import { DetailedActivity, StreamSet } from "strava";
 import { FRONTEND_URL } from "../config";
-import { read, remove, write } from "./redis";
+import { hget, hset, read, remove, write } from "./redis";
+
+const STRAVA_ACTIVITIES = createKey("strava-activities");
+const LOOKUP_KEY = createKey("lookup");
 
 export type Share = ShareStravaActivity;
 
@@ -25,6 +28,9 @@ export interface ShareStravaActivity {
   streams: StreamSet;
 }
 
+/**
+ * @deprecated
+ */
 type ShareLookup = { [hash: string]: string };
 
 export function getShareUrl(id: string) {
@@ -35,6 +41,9 @@ function createKey(shareId: string): string {
   return `share:${shareId}`;
 }
 
+/**
+ * @deprecated
+ */
 function createHash(share: Omit<Share, "id">): string {
   switch (share.type) {
     case "strava-activity":
@@ -42,25 +51,35 @@ function createHash(share: Omit<Share, "id">): string {
   }
 }
 
-const LOOKUP_KEY = createKey("lookup");
+export async function writeShare(
+  shareWithoutId: Omit<Share, "id">
+): Promise<Share> {
+  const lookupShareId = await hget(
+    STRAVA_ACTIVITIES,
+    shareWithoutId.activity.id.toString()
+  );
+  if (lookupShareId) {
+    return (await readShare(lookupShareId))!;
+  }
 
-export async function writeShare(share: Omit<Share, "id">): Promise<Share> {
-  const hash = createHash(share);
   const lookup = (await read<ShareLookup>(LOOKUP_KEY)) ?? {};
-
+  const hash = createHash(shareWithoutId);
   if (lookup[hash]) {
+    // remove once migrated
     const id = lookup[hash];
+    await hset(STRAVA_ACTIVITIES, shareWithoutId.activity.id.toString(), id);
     return (await readShare(id))!;
   } else {
     const id = short.generate();
-    lookup[hash] = id;
+    const shareWithId = { ...shareWithoutId, id };
+    await write(createKey(id), shareWithId);
+    await hset(STRAVA_ACTIVITIES, shareWithoutId.activity.id.toString(), id);
 
-    const itemWithId = { ...share, id };
+    // remove once migrated
+    delete lookup[hash];
+    await write<ShareLookup>(LOOKUP_KEY, lookup);
 
-    await write(createKey(id), itemWithId);
-    await write(LOOKUP_KEY, lookup);
-
-    return itemWithId;
+    return shareWithId;
   }
 }
 
@@ -72,6 +91,8 @@ export async function readShare(shareId: string): Promise<Share | undefined> {
 
   switch (share.type) {
     case "strava-activity":
+      // remove once migrated
+      await hset(STRAVA_ACTIVITIES, share.activity.id.toString(), shareId);
       return share;
   }
 }
