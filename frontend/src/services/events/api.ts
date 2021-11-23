ef0c269f4e85b209edf964f7181bf33b2259a9ae
Andi Pätzold <github@andipaetzold.com>
2021-11-23 23:19:41 +0100

    feat cache public zwift api

--- frontend/src/services/events/api.ts
+++ frontend/src/services/events/api.ts
@@ -1,30 +1,18 @@
 import axios from "axios";
+import { createAxiosCacheAdapter } from "../axios-cache-adapter";
 import { ZwiftEvent } from "./types";
-
-let listCache: Promise<ZwiftEvent[]> | undefined;
 
 const api = axios.create({
   baseURL: "https://us-or-rly101.zwift.com/api",
+  adapter: createAxiosCacheAdapter(),
 });
 
 export async function fetchEvents(): Promise<ZwiftEvent[]> {
-  if (!listCache) {
-    listCache = api
-      .get<ZwiftEvent[]>("/public/events/upcoming")
-      .then((r) => r.data);
-  }
-
-  return await listCache;
+  const response = await api.get<ZwiftEvent[]>("/public/events/upcoming");
+  return response.data;
 }
 
 export async function fetchEvent(eventId: string): Promise<ZwiftEvent> {
-  if (listCache) {
-    const allEvents = await listCache;
-    const eventInList = allEvents.find((e) => e.id.toString() === eventId);
-    if (eventInList) {
-      return eventInList;
-    }
-  }
-
-  return api.get<ZwiftEvent>(`/public/events/${eventId}`).then((r) => r.data);
+  const response = await api.get<ZwiftEvent>(`/public/events/${eventId}`);
+  return response.data;
 }
