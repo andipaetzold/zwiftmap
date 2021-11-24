d6b33805510be19e6f53dac89d3d089859fe86b6
Andi Pätzold <github@andipaetzold.com>
2021-11-24 18:11:15 +0100

    chore: automatically start ngrok tunnel in dev

---
+++ backend/scripts/start-web.mjs
@@ -0,0 +1,42 @@
+import { spawn } from "child_process";
+import "dotenv/config";
+import ngrok from "ngrok";
+import TscWatchClient from "tsc-watch/client.js";
+
+
+const url = await ngrok.connect({
+  proto: "http",
+  region: "eu",
+  addr: process.env.PORT,
+});
+
+console.log(`ngrok tunnel opened at: ${url}`);
+console.log("Open the ngrok dashboard at: http://localhost:4040");
+
+const watch = new TscWatchClient();
+
+let childProcess;
+function killChildProcess() {
+  if (childProcess) {
+    childProcess.kill("SIGINT");
+    childProcess = undefined;
+  }
+}
+
+watch.on("success", () => {
+  killChildProcess();
+
+  childProcess = spawn(`node`, ["./build-web/web"], {
+    stdio: "inherit",
+    env: {
+      ...process.env,
+      STRAVA_WEBHOOK_HOST: url,
+    },
+  });
+});
+
+watch.on("compile_errors", () => {
+  killChildProcess();
+});
+
+watch.start("--project", "web.tsconfig.json", "--preserveWatchOutput");
