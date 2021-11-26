fc1fb7de56a7f60b65e20a28ac3ee2dd25e0011e
Andi Pätzold <github@andipaetzold.com>
2021-11-26 19:12:08 +0100

    fix: send 404 if share does not exist

--- backend/src/web/handlers/share/get.ts
+++ backend/src/web/handlers/share/get.ts
@@ -4,5 +4,11 @@ import { readShare } from "../../../shared/persistence/share";
 export async function handleGetShare(req: Request, res: Response) {
   const shareId = req.params.shareId;
   const share = await readShare(shareId);
-  res.json(share);
+
+  if (!share) {
+    res.sendStatus(404);
+    return;
+  }
+
+  res.status(200).json(share);
 }
