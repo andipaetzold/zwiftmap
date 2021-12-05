08efc53dec20622a34689e172f40bee26358fcc8
Andi Pätzold <github@andipaetzold.com>
2021-12-06 00:48:23 +0100

    fix: hide workout chart on error

--- frontend/src/components/Sidebar/Content/Event/EventWorkoutChart.tsx
+++ frontend/src/components/Sidebar/Content/Event/EventWorkoutChart.tsx
@@ -1,4 +1,5 @@
 import { SimpleListItem } from "@react-md/list";
+import { useState } from "react";
 import { BACKEND_HOST } from "../../../../config";
 import { ZwiftEvent } from "../../../../services/events";
 
@@ -7,6 +8,12 @@ interface Props {
 }
 
 export function EventWorkoutChart({ event }: Props) {
+  const [error, setError] = useState(false);
+
+  if (error) {
+    return null;
+  }
+
   if (event.type !== "EVENT_TYPE_GROUP_WORKOUT") {
     return null;
   }
@@ -14,7 +21,12 @@ export function EventWorkoutChart({ event }: Props) {
   const url = new URL(`/events/${event.id}/workout`, BACKEND_HOST);
   return (
     <SimpleListItem>
-      <img src={url.toString()} alt="Workout Chart" width="100%" />
+      <img
+        src={url.toString()}
+        alt="Workout Chart"
+        width="100%"
+        onError={() => setError(true)}
+      />
     </SimpleListItem>
   );
 }
