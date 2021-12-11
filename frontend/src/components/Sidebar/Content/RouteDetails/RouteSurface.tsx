6752e69eca13c7c7d2e760a63280d52f658bc56b
Andi Pätzold <github@andipaetzold.com>
2021-12-12 00:59:38 +0100

    refactor: extract route surface components

--- frontend/src/components/Sidebar/Content/RouteDetails/RouteSurface.tsx
+++ frontend/src/components/Sidebar/Content/RouteDetails/RouteSurface.tsx
@@ -94,59 +94,73 @@ export function RouteSurface({ route }: Props) {
         Surfaces
       </ListSubheader>
 
-      <SimpleListItem aria-hidden="true">
-        <div style={{ width: "100%", height: 100 }}>
-          <ResponsiveContainer width="100%" height="100%">
-            <BarChart data={[surfacesWithDistance]} layout="vertical">
-              <XAxis
-                type="number"
-                allowDecimals={false}
-                tickCount={4}
-                domain={[0, "auto"]}
-                unit="km"
-              />
-              <YAxis
-                type="category"
-                dataKey="surface"
-                tick={false}
-                width={15}
-              />
-              {SURFACES.filter((s) => surfacesWithDistance[s] > 0).map((s) => (
-                <Bar
-                  key={s}
-                  dataKey={s}
-                  fill={SURFACE_CONSTANTS[s].color}
-                  label={SURFACE_CONSTANTS[s].label}
-                />
-              ))}
-            </BarChart>
-          </ResponsiveContainer>
-        </div>
-      </SimpleListItem>
-
-      <SimpleListItem aria-hidden="true">
-        <ul style={{ padding: 0, margin: 0 }}>
-          {SURFACES.filter((s) => surfacesWithDistance[s] > 0).map((s) => (
-            <li key={s} style={{ display: "block" }}>
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 32 32"
-                style={{
-                  display: "inline-block",
-                  verticalAlign: "middle",
-                  marginRight: "4px",
-                }}
-              >
-                <path fill={SURFACE_CONSTANTS[s].color} d="M0,4h32v24h-32z" />
-              </svg>
-              {SURFACE_CONSTANTS[s].label}:{" "}
-              <Distance distance={surfacesWithDistance[s]} /> (
-              {FORMAT_PERCENT.format(surfacesWithDistance[s] / route.distance)})
-            </li>
-          ))}
-        </ul>
-      </SimpleListItem>
+      <Chart data={surfacesWithDistance} />
+      <Legend data={surfacesWithDistance} distance={route.distance} />
     </List>
+  );
+}
+
+interface ChartProps {
+  data: Record<Surface, number>;
+}
+
+function Chart({ data }: ChartProps) {
+  return (
+    <SimpleListItem aria-hidden="true">
+      <div style={{ width: "100%", height: 100 }}>
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart data={[data]} layout="vertical">
+            <XAxis
+              type="number"
+              allowDecimals={false}
+              tickCount={4}
+              domain={[0, "auto"]}
+              unit="km"
+            />
+            <YAxis type="category" dataKey="surface" tick={false} width={15} />
+            {SURFACES.filter((s) => data[s] > 0).map((s) => (
+              <Bar
+                key={s}
+                dataKey={s}
+                fill={SURFACE_CONSTANTS[s].color}
+                label={SURFACE_CONSTANTS[s].label}
+              />
+            ))}
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+    </SimpleListItem>
+  );
+}
+
+interface LegendProps {
+  data: Record<Surface, number>;
+  distance: number;
+}
+
+function Legend({ data, distance }: LegendProps) {
+  return (
+    <SimpleListItem>
+      <ul style={{ padding: 0, margin: 0 }}>
+        {SURFACES.filter((s) => data[s] > 0).map((s) => (
+          <li key={s} style={{ display: "block" }}>
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 32 32"
+              style={{
+                display: "inline-block",
+                verticalAlign: "middle",
+                marginRight: "4px",
+              }}
+            >
+              <path fill={SURFACE_CONSTANTS[s].color} d="M0,4h32v24h-32z" />
+            </svg>
+            {SURFACE_CONSTANTS[s].label}: <Distance distance={data[s]} /> (
+            {FORMAT_PERCENT.format(data[s] / distance)})
+          </li>
+        ))}
+      </ul>
+    </SimpleListItem>
   );
 }
