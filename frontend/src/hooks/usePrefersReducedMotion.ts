48da868051a61cbe7f62a7717871de3e519dd597
Andi Pätzold <github@andipaetzold.com>
2021-12-11 17:20:19 +0100

    refactor: use react-responsive for usePrefersReducedMotion

--- frontend/src/hooks/usePrefersReducedMotion.ts
+++ frontend/src/hooks/usePrefersReducedMotion.ts
@@ -1,25 +1,7 @@
-import { useEffect, useState } from "react";
-
-const reducedMotionQuery = window.matchMedia(
-  "(prefers-reduced-motion: reduce)"
-);
+import { useMediaQuery } from "react-responsive";
 
 export function usePrefersReducedMotion(): boolean {
-  const [state, setState] = useState(reducedMotionQuery.matches);
-
-  useEffect(() => {
-    const listener = () => {
-      setState(reducedMotionQuery.matches);
-    };
-
-    if ("addEventListener" in reducedMotionQuery) {
-      reducedMotionQuery.addEventListener("change", listener);
-      return () => reducedMotionQuery.removeEventListener("change", listener);
-    } else if ("addListener" in reducedMotionQuery) {
-      reducedMotionQuery.addListener(listener);
-      return () => reducedMotionQuery.removeListener(listener);
-    }
-  }, []);
-
-  return state;
+  return useMediaQuery({
+    query: "(prefers-reduced-motion: reduce)",
+  });
 }
