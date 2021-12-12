0d3f8fb92804f41fc9cca51a3ef18bdf26de8c6c
Andi Pätzold <github@andipaetzold.com>
2021-12-12 02:05:16 +0100

    feat: richmond surfaces

--- frontend/src/worldConfig.ts
+++ frontend/src/worldConfig.ts
@@ -180,6 +180,7 @@ const WORLD_CONFIG_PARIS: WorldConfig = {
   backgroundColor: "#b9b9b9",
   surfaces: [
     {
+      // whole world
       surface: SURFACE_COBBLES,
       polygon: [
         [48.9058, 2.2561],
@@ -199,7 +200,46 @@ const WORLD_CONFIG_RICHMOND: WorldConfig = {
   ],
   image: richmondMap,
   backgroundColor: "#7c9938",
-  surfaces: [],
+  surfaces: [
+    {
+      // top left
+      surface: SURFACE_COBBLES,
+      polygon: [
+        [37.552037, -77.457716],
+        [37.552969, -77.456601],
+        [37.560331, -77.466994],
+        [37.557636, -77.469665],
+        [37.552258, -77.458398],
+        [37.552037, -77.457716],
+      ],
+    },
+    {
+      // KOM
+      surface: SURFACE_COBBLES,
+      polygon: [
+        [37.527933, -77.419275],
+        [37.527265, -77.418585],
+        [37.526495, -77.417553],
+        [37.527067, -77.416833],
+        [37.527464, -77.417485],
+        [37.527662, -77.417273],
+        [37.528059, -77.417804],
+        [37.528486, -77.418464],
+        [37.527933, -77.419275],
+      ],
+    },
+    {
+      // 23rd street
+      surface: SURFACE_COBBLES,
+      polygon: [
+        [37.531275, -77.422526],
+        [37.530939, -77.421834],
+        [37.531777, -77.421024],
+        [37.532084, -77.421673],
+        [37.531275, -77.422526],
+      ],
+    },
+  ],
 };
 
 const WORLD_CONFIG_WATOPIA: WorldConfig = {
