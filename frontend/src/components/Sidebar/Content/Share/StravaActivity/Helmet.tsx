8de2c461de9997dae4e61ee91daee75fa83d3fc8
Andi Pätzold <github@andipaetzold.com>
2021-12-15 00:52:36 +0100

    fix: change property to name for twitter meta tags

--- frontend/src/components/Sidebar/Content/Share/StravaActivity/Helmet.tsx
+++ frontend/src/components/Sidebar/Content/Share/StravaActivity/Helmet.tsx
@@ -13,12 +13,12 @@ export function SharedStravaActivityHelmet({ share, imageUrl }: Props) {
       <title>{share.activity.name}</title>
       <meta name="description" content={description} />
 
+      {imageUrl && (
+        <meta name="twitter:card" content="summary_large_image" />
+      )}
+
       <meta property="og:title" content={`${share.activity.name} - ZwiftMap`} />
       <meta property="og:description" content={description} />
-
-      {imageUrl && (
-        <meta property="twitter:card" content="summary_large_image" />
-      )}
       {imageUrl && <meta property="og:image" content={imageUrl} />}
       {imageUrl && <meta property="og:image:height" content="1080" />}
       {imageUrl && <meta property="og:image:width" content="1920" />}
