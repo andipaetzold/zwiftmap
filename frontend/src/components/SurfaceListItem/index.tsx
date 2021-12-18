096df3799a8ee1fad7f458b7537bf6e8c037fd29
Andi Pätzold <github@andipaetzold.com>
2021-12-18 23:17:38 +0100

    refactor: extract surface logic

--- frontend/src/components/SurfaceListItem/index.tsx
+++ frontend/src/components/SurfaceListItem/index.tsx
@@ -1,21 +1,9 @@
 import { List, SimpleListItem } from "@react-md/list";
 import { LatLngTuple } from "leaflet";
 import { SURFACE_CONSTANTS } from "../../constants";
-import {
-  SurfaceType,
-  SURFACE_TYPES,
-  SURFACE_TYPE_BRICK,
-  SURFACE_TYPE_COBBLES,
-  SURFACE_TYPE_DIRT,
-  SURFACE_TYPE_GRASS,
-  SURFACE_TYPE_SNOW,
-  SURFACE_TYPE_TARMAC,
-  SURFACE_TYPE_WOOD,
-  WorldConfigSurface,
-} from "../../types";
+import { SurfaceType, SURFACE_TYPES, WorldConfigSurface } from "../../types";
+import { getSurfaceStats, getSurfaceStream } from "../../util/surface";
 import { Distance } from "../Distance";
-import booleanPointInPolygon from "@turf/boolean-point-in-polygon";
-import { point as turfPoint, polygon as turfPolygon } from "@turf/helpers";
 
 const FORMAT_PERCENT = new Intl.NumberFormat("en-US", {
   minimumFractionDigits: 0,
@@ -36,33 +24,11 @@ export function SurfaceListItem({
   distance,
   surfaces,
 }: Props) {
-  const worldSurfaces = surfaces.map((worldSurface) => ({
-    ...worldSurface,
-    polygon: turfPolygon([worldSurface.polygon]),
-  }));
-
-  const routeSurfaces = latLngStream
-    .map((latlng) => turfPoint(latlng))
-    .map(
-      (point) =>
-        worldSurfaces.find((ws) => booleanPointInPolygon(point, ws.polygon))
-          ?.type ?? SURFACE_TYPE_TARMAC
-    );
-
-  const surfaceTypesWithDistance: Record<SurfaceType, number> = {
-    [SURFACE_TYPE_TARMAC]: 0,
-    [SURFACE_TYPE_BRICK]: 0,
-    [SURFACE_TYPE_WOOD]: 0,
-    [SURFACE_TYPE_COBBLES]: 0,
-    [SURFACE_TYPE_SNOW]: 0,
-    [SURFACE_TYPE_DIRT]: 0,
-    [SURFACE_TYPE_GRASS]: 0,
-  };
-
-  for (let i = 1; i < routeSurfaces.length; ++i) {
-    surfaceTypesWithDistance[routeSurfaces[i]] +=
-      (distancStream[i] - distancStream[i - 1]) / 1_000;
-  }
+  const surfaceStream = getSurfaceStream(latLngStream, surfaces);
+  const surfaceTypesWithDistance = getSurfaceStats(
+    distancStream,
+    surfaceStream
+  );
 
   return (
     <List style={{ marginTop: 0, marginBottom: 0 }}>
---
+++ frontend/src/util/surface.ts
@@ -0,0 +1,55 @@
+import booleanPointInPolygon from "@turf/boolean-point-in-polygon";
+import { point as turfPoint, polygon as turfPolygon } from "@turf/helpers";
+import { LatLngTuple } from "leaflet";
+import {
+  SurfaceType,
+  SURFACE_TYPE_BRICK,
+  SURFACE_TYPE_COBBLES,
+  SURFACE_TYPE_DIRT,
+  SURFACE_TYPE_GRASS,
+  SURFACE_TYPE_SNOW,
+  SURFACE_TYPE_TARMAC,
+  SURFACE_TYPE_WOOD,
+  WorldConfigSurface,
+} from "../types";
+
+export function getSurfaceStream(
+  latLngStream: LatLngTuple[],
+  surfaces: WorldConfigSurface[]
+): SurfaceType[] {
+  const turfedWorldSurfaces = surfaces.map((worldSurface) => ({
+    ...worldSurface,
+    polygon: turfPolygon([worldSurface.polygon]),
+  }));
+
+  return latLngStream
+    .map((latlng) => turfPoint(latlng))
+    .map(
+      (point) =>
+        turfedWorldSurfaces.find((worldSurface) =>
+          booleanPointInPolygon(point, worldSurface.polygon)
+        )?.type ?? SURFACE_TYPE_TARMAC
+    );
+}
+
+export function getSurfaceStats(
+  distanceStream: number[],
+  surfaceStream: SurfaceType[]
+): Record<SurfaceType, number> {
+  const result: Record<SurfaceType, number> = {
+    [SURFACE_TYPE_TARMAC]: 0,
+    [SURFACE_TYPE_BRICK]: 0,
+    [SURFACE_TYPE_WOOD]: 0,
+    [SURFACE_TYPE_COBBLES]: 0,
+    [SURFACE_TYPE_SNOW]: 0,
+    [SURFACE_TYPE_DIRT]: 0,
+    [SURFACE_TYPE_GRASS]: 0,
+  };
+
+  for (let i = 1; i < distanceStream.length; ++i) {
+    result[surfaceStream[i]] +=
+      (distanceStream[i] - distanceStream[i - 1]) / 1_000;
+  }
+
+  return result;
+}
