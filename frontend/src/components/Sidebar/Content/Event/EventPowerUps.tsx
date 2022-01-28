472fe64e799492098bb6c20d90ca9198091c7c4e
Andi Pätzold <github@andipaetzold.com>
2022-01-28 21:55:46 +0100

    feat: add default power up distribution

--- frontend/src/components/Sidebar/Content/Event/EventPowerUps.tsx
+++ frontend/src/components/Sidebar/Content/Event/EventPowerUps.tsx
@@ -19,8 +19,8 @@ interface PowerUp {
 const POWER_UPS: Record<number, PowerUp> = {
   0: { name: "Feather", color: "#a1cb2a" },
   1: { name: "Draft Boost", color: "#1983eb" },
-  2: { name: "Small Bonus (10 XP)", color: "#de45c9" },
-  3: { name: "Large Bonus (250 XP)", color: "#da3e49" },
+  2: { name: "Small Bonus", color: "#de45c9" },
+  3: { name: "Large Bonus", color: "#da3e49" },
   4: { name: "Burrito", color: "#a87b4b" },
   5: { name: "Aero Boost", color: "#8bf4f6" },
   6: { name: "Ghost", color: "#0a0d15" },
@@ -28,30 +28,38 @@ const POWER_UPS: Record<number, PowerUp> = {
   8: { name: "Anvil", color: "#4a4a4a" },
 };
 
+/**
+ * source: https://zwiftinsider.com/distribution-of-zwift-powerups/
+ */
+const DEFAULT_DISTRIBUTION = {
+  0: 25,
+  1: 25,
+  2: 29,
+  3: 1,
+  5: 20,
+};
+
 interface Props {
   event: ZwiftEvent;
 }
 
 export function EventPowerUps({ event }: Props) {
+  if (event.rulesSet.includes("NO_POWERUPS")) {
+    return null;
+  }
+
   const tag = event.tags.find((tag) => tag.startsWith("powerup_percent="));
-
-  if (!tag) {
-    return null;
-  }
-
-  const data = Object.fromEntries(
-    chunk(
-      tag
-        .substring('powerup_percent="'.length, tag.length - 1)
-        .split(",")
-        .map((v) => +v),
-      2
-    )
-  );
-
-  if (data.length === 0) {
-    return null;
-  }
+  const distribution = tag
+    ? Object.fromEntries(
+        chunk(
+          tag
+            .substring('powerup_percent="'.length, tag.length - 1)
+            .split(",")
+            .map((v) => +v),
+          2
+        )
+      )
+    : DEFAULT_DISTRIBUTION;
 
   return (
     <>
@@ -60,14 +68,20 @@ export function EventPowerUps({ event }: Props) {
       <SimpleListItem aria-hidden="true">
         <div style={{ width: "100%", height: 100 }}>
           <ResponsiveContainer width="100%" height="100%">
-            <BarChart width={600} height={300} data={[data]} layout="vertical">
+            <BarChart
+              width={600}
+              height={300}
+              data={[distribution]}
+              layout="vertical"
+            >
               <XAxis type="number" hide />
               <YAxis type="category" dataKey="name" hide />
               <Legend />
               {Object.entries(POWER_UPS)
-                .filter(([key]) => data[key])
+                .filter(([key]) => distribution[key])
                 .map(([key, { name, color }]) => (
                   <Bar
+                    key={key}
                     dataKey={key}
                     stackId="stack"
                     name={name}
