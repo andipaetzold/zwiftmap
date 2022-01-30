f28d4444c0e62d821499f5d88a7ece14f97eb8ec
Andi Pätzold <github@andipaetzold.com>
2022-01-30 23:02:55 +0100

    feat: improve styling

--- frontend/src/components/Sidebar/Content/Navigation/index.tsx
+++ frontend/src/components/Sidebar/Content/Navigation/index.tsx
@@ -1,9 +1,14 @@
 import { List, SimpleListItem } from "@react-md/list";
+import { Typography } from "@react-md/typography";
 
 export function Navigation() {
   return (
     <List>
-      <SimpleListItem>Navigation</SimpleListItem>
+      <SimpleListItem>
+        <Typography type="headline-6" style={{ margin: 0 }}>
+          Navigation
+        </Typography>
+      </SimpleListItem>
     </List>
   );
 }
