f693a9572ee8321f3ffa0af9f994215bb787730c
Andi Pätzold <github@andipaetzold.com>
2022-01-31 19:17:31 +0100

    feat: remove start finish node from crit city

--- frontend/src/constants/roads/crit-city/index.ts
+++ frontend/src/constants/roads/crit-city/index.ts
@@ -1,9 +1,8 @@
 import { Roads } from "../../../services/Roads";
-import edgeStartToPaddock from "./edges/start-paddock";
+import edgeLap from "./edges/lap";
 
 const ROADS = new Roads();
 
-const START_FINISH = ROADS.createNode([-10.383696, 165.801973, 119.4]);
 const PADDOCK_EXIT = ROADS.createNode([-10.383915, 165.801973, 119.4]);
 const PADDOCK_1 = ROADS.createNode([-10.384382, 165.803166, 119.4]);
 const PADDOCK_2 = ROADS.createNode([-10.383472, 165.803158, 119.4]);
@@ -13,8 +12,7 @@ const PADDOCK_CENTER = ROADS.createNode([-10.383925, 165.803517, 119.4]);
 const PADDOCK_ROW_FRONT = ROADS.createNode([-10.383915, 165.80272, 119.4]);
 const PADDOCK_ROW_BACK = ROADS.createNode([-10.383919, 165.803081, 119.4]);
 
-ROADS.createEdge(START_FINISH, PADDOCK_EXIT, edgeStartToPaddock);
-ROADS.createEdge(PADDOCK_EXIT, START_FINISH, []);
+ROADS.createEdge(PADDOCK_EXIT, PADDOCK_EXIT, edgeLap);
 ROADS.createEdge(PADDOCK_ROW_FRONT, PADDOCK_EXIT, []);
 ROADS.createEdge(PADDOCK_1, PADDOCK_ROW_FRONT, []);
 ROADS.createEdge(PADDOCK_2, PADDOCK_ROW_FRONT, []);
