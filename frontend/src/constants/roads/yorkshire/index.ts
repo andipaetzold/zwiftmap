c8d6451a9e79b4f5bed871567cd115c2d6d3bcc0
Andi Pätzold <github@andipaetzold.com>
2022-01-31 18:45:20 +0100

    feat: remove start finish node from yorkshire

--- frontend/src/constants/roads/yorkshire/index.ts
+++ frontend/src/constants/roads/yorkshire/index.ts
@@ -5,7 +5,6 @@ import edgeBigLoopNorth from "./edges/big-loop-north";
 
 const ROADS = new Roads();
 
-const START_FINISH = ROADS.createNode([53.989692, -1.541149, 125.8]);
 const PADDOCK_EXIT = ROADS.createNode([53.990383, -1.541467, 0]); // TODO
 const PADDOCK_1 = ROADS.createNode([53.990773, -1.544834, 0]); // TODO
 const PADDOCK_2 = ROADS.createNode([53.989051, -1.54404, 0]); // TODO
@@ -17,8 +16,7 @@ const PADDOCK_ROW_BACK = ROADS.createNode([53.989928, -1.544358, 0]); // TODO
 const JUNCTION_NORTH = ROADS.createNode([53.993835, -1.546892, 0]); // TODO
 const JUNCTION_SOUTH = ROADS.createNode([53.993627, -1.547179, 108.4]);
 
-ROADS.createEdge(PADDOCK_EXIT, START_FINISH, []);
-ROADS.createEdge(JUNCTION_SOUTH, START_FINISH, edgeBigLoopSouth);
+ROADS.createEdge(JUNCTION_SOUTH, PADDOCK_EXIT, edgeBigLoopSouth);
 ROADS.createEdge(PADDOCK_EXIT, JUNCTION_NORTH, edgeBigLoopNorth);
 ROADS.createEdge(JUNCTION_SOUTH, JUNCTION_NORTH, edgeSmallLoop);
 ROADS.createEdge(JUNCTION_NORTH, JUNCTION_SOUTH, []);
