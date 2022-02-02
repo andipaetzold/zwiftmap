600d7ff52daabb587453277baa52084e42cad06a
Andi Pätzold <github@andipaetzold.com>
2022-02-02 21:46:20 +0100

    Watopia Roads (#485)

---
+++ frontend/src/constants/roads/watopia/edges/fuego-south-roundabout-2.ts
@@ -0,0 +1,10 @@
+import { RoadPosition } from "../../../../services/Roads";
+
+const stream: RoadPosition[] = [
+  [-11.661876, 166.983948, 12.8],
+  [-11.66199, 166.983787, 12.6],
+  [-11.662092, 166.983604, 12.6],
+  [-11.662201, 166.983433, 12.8],
+];
+
+export default stream;
---
+++ frontend/src/constants/roads/watopia/edges/main-south-junction-2.ts
@@ -0,0 +1,8 @@
+import { RoadPosition } from "../../../../services/Roads";
+
+const stream: RoadPosition[] = [
+  [-11.648115, 166.952416, 15.8],
+  [-11.648055, 166.952598, 15.6],
+];
+
+export default stream;
