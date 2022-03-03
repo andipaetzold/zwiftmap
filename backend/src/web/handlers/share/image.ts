677a77fe91eb7aa924c7fcdffeb4b078df5ffe2c
Andi Pätzold <github@andipaetzold.com>
2022-03-03 15:42:12 +0100

    feat: route is always visible in viewport

--- backend/src/web/handlers/share/image.ts
+++ backend/src/web/handlers/share/image.ts
@@ -55,7 +55,7 @@ export async function handleGETShareImage(req: Request, res: Response) {
   // longitude: x
   const content = [
     '<?xml version="1.0" encoding="UTF-8"?>',
-    `<svg viewBox="${viewBox}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${width}" height="${height}">`,
+    `<svg viewBox="${viewBox}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${width}" height="${height}" preserveAspectRatio="xMidYMid meet">`,
     await getWorldImageTag(world),
     `<polyline points="${share.streams.latlng.data
       .map((latlng) => toXAndY(latlng))
@@ -104,12 +104,12 @@ async function getWorldImageTag(world: World) {
   const height = diff(xAndYBounds[0][1], xAndYBounds[1][1]);
 
   const buffer = await worldImages[world.slug];
-  return `<image  x="${x}" y="${y}" width="${width}" height="${height}" xlink:href="${bufferToDataUrl(
+  return `<image x="${x}" y="${y}" width="${width}" height="${height}" xlink:href="${bufferToDataUrl(
     buffer
-  )}"  />`;
+  )}" />`;
 }
 
-const PADDING = 0.001;
+const PADDING = 0.0005;
 function bbox(stream: LatLng[]): [LatLng, LatLng] {
   const result: [LatLng, LatLng] = [
     [Infinity, Infinity],
@@ -132,7 +132,7 @@ function bbox(stream: LatLng[]): [LatLng, LatLng] {
   }
 
   return [
-    [result[0][0] + PADDING, result[0][1] - PADDING],
-    [result[1][0] - PADDING, result[1][1] + PADDING],
+    [result[0][0] - PADDING, result[0][1] - PADDING],
+    [result[1][0] + PADDING, result[1][1] + PADDING],
   ];
 }
