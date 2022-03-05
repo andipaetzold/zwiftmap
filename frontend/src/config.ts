ab344d76373798e167737e2eb9a5ebb32d4a3b32
Andi Pätzold <github@andipaetzold.com>
2022-03-05 12:43:44 +0100

    fix: replace NODE_ENV with MODE

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,3 @@
 export const BACKEND_HOST = import.meta.env.VITE_BACKEND_HOST as string;
 export const GIT_SHA = import.meta.env.VITE_GIT_SHA as string;
-export const ENVIRONMENT = import.meta.env.NODE_ENV as
-  | "development"
-  | "production";
+export const ENVIRONMENT = import.meta.env.MODE as "development" | "production";
