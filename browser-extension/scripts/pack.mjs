3a1bf085e9fba7bbebc92d610799d3786ad228f6
Andi Pätzold <github@andipaetzold.com>
2022-03-17 13:36:27 +0100

    chore: add script to pack extension zips

--- browser-extension/scripts/pack.mjs
+++ browser-extension/scripts/pack.mjs
@@ -1,9 +1,52 @@
 import Zip from "adm-zip";
 
-const zip = new Zip();
+function createChromeExt() {
+  const zip = new Zip();
 
-zip.addLocalFile("manifest.json");
-zip.addLocalFolder("assets", "assets");
-zip.addLocalFolder("dist", "dist");
+  zip.addLocalFile("manifest.json");
+  zip.addLocalFolder("assets", "assets");
+  zip.addLocalFolder("dist", "dist");
 
-zip.writeZip("extension.zip");
+  zip.writeZip("extension-chrome.zip");
+}
+function createFirefoxExt() {
+  const zip = new Zip();
+
+  zip.addLocalFile("manifest.v2.json", undefined, "manifest.json");
+  zip.addLocalFolder("assets", "assets");
+  zip.addLocalFolder("dist", "dist");
+
+  zip.writeZip("extension-firefox.zip");
+}
+
+function createChromeSource() {
+  const zip = new Zip();
+
+  zip.addLocalFile("manifest.json");
+  zip.addLocalFile("package.json");
+  zip.addLocalFile("package-lock.json");
+  zip.addLocalFile("README.md");
+  zip.addLocalFolder("assets", "assets");
+  zip.addLocalFolder("src", "src");
+
+  zip.writeZip("source-chrome.zip");
+}
+
+function createFirefoxSource() {
+  const zip = new Zip();
+
+  zip.addLocalFile("manifest.v2.json", undefined, "manifest.json");
+  zip.addLocalFile("package.json");
+  zip.addLocalFile("package-lock.json");
+  zip.addLocalFile("README.md");
+  zip.addLocalFolder("assets", "assets");
+  zip.addLocalFolder("src", "src");
+
+  zip.writeZip("source-firefox.zip");
+}
+
+createChromeExt();
+createFirefoxExt();
+
+createChromeSource();
+createFirefoxSource();
