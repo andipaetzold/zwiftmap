5823b88613ba7a14f0db85a6b885f1ba0ebfe25e
Andi Pätzold <github@andipaetzold.com>
2022-03-28 23:15:23 +0200

    feat: remove legacy settings loader

--- frontend/src/hooks/useSettings.ts
+++ frontend/src/hooks/useSettings.ts
@@ -3,10 +3,13 @@ import { persist } from "zustand/middleware";
 import { Sport } from "zwift-data";
 import { Overlay, Settings, Theme, Units } from "../types";
 
-/**
- * @deprecated
- */
-const LOCAL_STORAGE_KEY_LEGACY = "settings";
+const DEFAULT_SETTINGS: Settings = {
+  sport: "cycling",
+  units: "metric",
+  theme: "system",
+  overlay: "segments",
+};
+
 const LOCAL_STORAGE_KEY = "settings-store";
 
 interface SettingStore extends Settings {
@@ -18,41 +21,13 @@ interface SettingStore extends Settings {
 
 export const useSettings = create<SettingStore>(
   persist(
-    (set) => {
-      const legacySettings = loadLegacySettings();
-      return {
-        ...legacySettings,
-        setSport: (sport) => set(() => ({ sport })),
-        setUnits: (units) => set(() => ({ units })),
-        setTheme: (theme) => set(() => ({ theme })),
-        setOverlay: (overlay) => set(() => ({ overlay })),
-      };
-    },
+    (set) => ({
+      ...DEFAULT_SETTINGS,
+      setSport: (sport) => set(() => ({ sport })),
+      setUnits: (units) => set(() => ({ units })),
+      setTheme: (theme) => set(() => ({ theme })),
+      setOverlay: (overlay) => set(() => ({ overlay })),
+    }),
     { name: LOCAL_STORAGE_KEY }
   )
 );
-
-/**
- * @deprecated
- */
-function loadLegacySettings(): Settings {
-  try {
-    const rawSettings = localStorage.getItem(LOCAL_STORAGE_KEY_LEGACY) ?? "{}";
-    localStorage.removeItem(LOCAL_STORAGE_KEY_LEGACY);
-    const settings: Partial<Settings> = JSON.parse(rawSettings);
-
-    return {
-      sport: settings.sport ?? "cycling",
-      units: settings.units ?? "metric",
-      theme: settings.theme ?? "system",
-      overlay: settings.overlay ?? "segments",
-    };
-  } catch {
-    return {
-      sport: "cycling",
-      units: "metric",
-      theme: "system",
-      overlay: "segments",
-    };
-  }
-}
