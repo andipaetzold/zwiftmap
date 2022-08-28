29dbee5f8cf34c63c886bcbc57dbffa592abd8a1
Andi Pätzold <github@andipaetzold.com>
2022-08-28 19:11:53 +0200

    feat: move StravaToken table to firestore

--- backend/src/shared/persistence/stravaToken.ts
+++ backend/src/shared/persistence/stravaToken.ts
@@ -1,27 +1,31 @@
+import { firestore } from "./firestore";
 import { pool } from "./pg";
 import { StravaToken } from "./types";
 
+const COLLECTION_NAME = "strava-tokens";
+const collection = firestore.collection(COLLECTION_NAME);
+
 export async function writeStravaToken(stravaToken: StravaToken) {
-  await pool.query<
-    any,
-    [number, string, string, number, undefined | null | string[]]
-  >(
-    `INSERT INTO
-     "StravaToken"("athleteId", "token", "refreshToken", "expiresAt", "scope")
-     VALUES($1, $2, $3, $4, $5)
-     ON CONFLICT ("athleteId") DO UPDATE
-     SET "token" = $2, "refreshToken" = $3, "expiresAt" = $4, "scope" = $5`,
-    [
-      stravaToken.athleteId,
-      stravaToken.token,
-      stravaToken.refreshToken,
-      stravaToken.expiresAt,
-      stravaToken.scope,
-    ]
-  );
+  await collection.doc(stravaToken.athleteId.toString()).set(stravaToken);
 }
 
 export async function readStravaToken(
+  athleteId: number
+): Promise<StravaToken | undefined> {
+  const doc = collection.doc(athleteId.toString());
+  const snap = await doc.get();
+
+  if (snap.exists) {
+    return snap.data()! as StravaToken;
+  }
+
+  return await readStravaTokenPg(athleteId);
+}
+
+/**
+ * @deprecated Remove once migration is done
+ */
+async function readStravaTokenPg(
   athleteId: number
 ): Promise<StravaToken | undefined> {
   const result = await pool.query<StravaToken, [number]>(
@@ -29,10 +33,25 @@ export async function readStravaToken(
     [athleteId]
   );
 
-  return result.rows[0];
+  if (result.rowCount === 0) {
+    return undefined;
+  }
+
+  const stravaToken = result.rows[0];
+  await writeStravaToken(stravaToken);
+  await removeStravaTokenPg(athleteId);
+  return stravaToken;
 }
 
 export async function removeStravaToken(athleteId: number): Promise<void> {
+  await collection.doc(athleteId.toString()).delete();
+  await removeStravaTokenPg(athleteId);
+}
+
+/**
+ * @deprecated Remove once migration is done
+ */
+async function removeStravaTokenPg(athleteId: number): Promise<void> {
   await pool.query('DELETE FROM "StravaToken" WHERE "athleteId" = $1', [
     athleteId,
   ]);
