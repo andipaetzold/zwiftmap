4ab371d136940e411825d5cf0beb07088fb1eb35
Andi Pätzold <github@andipaetzold.com>
2022-09-08 19:14:57 +0200

    feat: set min instances to 1

--- backend/src/web/handlers/index.ts
+++ backend/src/web/handlers/index.ts
@@ -1,6 +1,6 @@
+export * from "./ah/index.js";
 export * from "./auth/index.js";
 export * from "./events/index.js";
 export * from "./health/index.js";
 export * from "./share/index.js";
 export * from "./strava/index.js";
-
