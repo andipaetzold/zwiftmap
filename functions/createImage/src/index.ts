f6d3c4335183e2ba094c4cd10b6037f48a8fe8a3
Andi Pätzold <github@andipaetzold.com>
2022-09-24 22:32:31 +0200

    feat: implement functions

--- functions/createImage/src/index.ts
+++ functions/createImage/src/index.ts
@@ -1,5 +1,47 @@
 import { http } from "@google-cloud/functions-framework";
+import { Storage } from "@google-cloud/storage";
+import axios from "axios";
 
-http("createImage", (req, res) => {
+const storage = new Storage();
+
+/**
+ * TODO: move to shared package
+ */
+interface ImageQueueData {
+  type: "share";
+  shareId: string;
+
+  resolution: { width: number; height: number };
+  googleCloudStorage?: {
+    filename: string;
+  };
+}
+
+http("createImage", async (req, res) => {
+  const { shareId, resolution, googleCloudStorage } =
+    req.body as ImageQueueData;
+
+  if (!googleCloudStorage) {
+    res.sendStatus(200);
+    return;
+  }
+
+  const url = `https://api2.zwiftmap.com/share/${shareId}/image?width=${resolution.width}&height=${resolution.height}`;
+  const response = await axios.get(url, {
+    responseType: "arraybuffer",
+  });
+
+  await storage
+    .bucket("images.zwiftmap.com")
+    .file(googleCloudStorage.filename)
+    .save(response.data, {
+      resumable: false,
+      metadata: {
+        metadata: {
+          environment: process.env.GCP_PROJECT ? "production" : "development",
+        },
+      },
+    });
+
   res.sendStatus(200);
 });
