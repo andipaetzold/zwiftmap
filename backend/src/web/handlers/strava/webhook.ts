d6d74cb2a97856c47617f2923c838ac7213736dd
Andi Pätzold <github@andipaetzold.com>
2022-10-03 23:25:14 +0200

    fix: remove webhook state

--- backend/src/web/handlers/strava/webhook.ts
+++ backend/src/web/handlers/strava/webhook.ts
@@ -1,25 +1,11 @@
 import { Request, Response } from "express";
 import { handleStravaWebhookEvent } from "../../../shared/services/strava/index.js";
 import { WebhookEvent } from "../../../shared/types.js";
-import { getWebhookSubscriptionId } from "../../state.js";
 
 export async function handleWebhook(req: Request, res: Response) {
   const webhookEvent = req.body;
   if (!WebhookEvent.guard(webhookEvent)) {
     res.sendStatus(400);
-    return;
-  }
-
-  const webhookSubscriptionId = getWebhookSubscriptionId();
-  if (webhookSubscriptionId === undefined) {
-    console.warn("Webhooks are not ready yet");
-    res.sendStatus(503);
-    return;
-  }
-
-  if (webhookEvent.subscription_id !== webhookSubscriptionId) {
-    console.warn("Wrong subscription id");
-    res.sendStatus(403);
     return;
   }
 
--- backend/src/web/state.ts
+++
@@ -1,8 +0,0 @@
-let webhookSubscriptionId: number | undefined;
-export function setWebhookSubscriptionId(subscriptionId: number | undefined) {
-  webhookSubscriptionId = subscriptionId;
-}
-
-export function getWebhookSubscriptionId(): number | undefined {
-  return webhookSubscriptionId;
-}
