93748a0ec6f63758cc43ad1eefb995b9302e0a6d
Andi Pätzold <github@andipaetzold.com>
2022-10-04 20:39:51 +0200

    refactor: remove unused code

--- functions/handleStravaWebhook/dist/index.js
+++
@@ -1,4 +0,0 @@
-import { http } from "@google-cloud/functions-framework";
-http("createImage", (req, res) => {
-    res.sendStatus(200);
-});
--- functions/handleStravaWebhook/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { http } from "@google-cloud/functions-framework";
-
-http("handleStravaWebhook", (req, res) => {
-  res.sendStatus(200);
-});
