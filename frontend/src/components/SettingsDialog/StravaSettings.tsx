bf59142e434dc1226d3d989780011c535d3c2720
Andi Pätzold <github@andipaetzold.com>
2022-10-20 20:42:56 +0200

    feat: show strava settings while logged out

--- frontend/src/components/SettingsDialog/StravaSettings.tsx
+++ frontend/src/components/SettingsDialog/StravaSettings.tsx
@@ -1,24 +1,38 @@
 import { Switch } from "@react-md/form";
+import { Typography } from "@react-md/typography";
 import { useStravaSettings } from "../../react-query";
+import { StravaSettings as StravaSettingsType } from "../../types";
+
+const DEFAULT_STRAVA_SETTINGS: StravaSettingsType = {
+  addLinkToActivityDescription: false,
+};
 
 export function StravaSettings() {
   const [settings, setSettings] = useStravaSettings();
 
-  if (settings === null) {
-    return null;
-  }
-
   return (
-    <Switch
-      id="settings-strava-add-link"
-      label="Automatically add ZwiftMap links to Strava activities"
-      checked={settings.addLinkToActivityDescription}
-      onChange={(e) =>
-        setSettings({
-          ...settings,
-          addLinkToActivityDescription: e.target.checked,
-        })
-      }
-    />
+    <>
+      <Switch
+        id="settings-strava-add-link"
+        label="Automatically add ZwiftMap links to Strava activities"
+        checked={
+          settings?.addLinkToActivityDescription ??
+          DEFAULT_STRAVA_SETTINGS.addLinkToActivityDescription
+        }
+        disabled={settings === null}
+        onChange={(e) =>
+          setSettings({
+            ...settings,
+            addLinkToActivityDescription: e.target.checked,
+          })
+        }
+      />
+
+      {settings === null && (
+        <Typography type="body-2">
+          Some options require you to be logged in via Strava.
+        </Typography>
+      )}
+    </>
   );
 }
