e26868e0f1802369119cce6507c15a4bfdd93b5a
Andi Pätzold <github@andipaetzold.com>
2022-10-22 00:36:53 +0200

    refactor: adjust fog stats query key

--- frontend/src/react-query/queryKeys.ts
+++ frontend/src/react-query/queryKeys.ts
@@ -8,7 +8,8 @@ export const queries = {
   worldRoads: (world: WorldSlug) => ["worlds", world, "roads"] as const,
   worldRoadsBuffered: (world: WorldSlug) =>
     ["worlds", world, "roads-buffered"] as const,
-  stravaFogStats: (world: WorldSlug) => ["worlds", world, "fog"] as const,
   stravaFogGeoJSON: (world: WorldSlug) =>
     ["worlds", world, "fog", "geojson"] as const,
+  stravaFogStats: (world: WorldSlug) =>
+    ["worlds", world, "fog", "stats"] as const,
 };
