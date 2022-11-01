8e824a7509ab6f9cc836dfea21fa84df400e0137
Andi Pätzold <github@andipaetzold.com>
2022-11-01 22:49:49 +0100

    fix: remove frontend tracing
    I don't look at the data anyway

--- frontend/src/sentry.ts
+++ frontend/src/sentry.ts
@@ -1,15 +1,5 @@
 import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
-import { BACKEND_HOST, GIT_SHA } from "./config";
-import "./index.scss";
-import {
-  PATTERN_CUSTOM_ROUTE,
-  PATTERN_EVENT,
-  PATTERN_ROUTE_OR_SEGMENT,
-  PATTERN_SHARED_ITEM,
-  PATTERN_STRAVA_ACTIVITY,
-  PATTERN_WORLD,
-} from "./services/routing";
+import { GIT_SHA } from "./config";
 
 export function initSentry() {
   Sentry.init({
@@ -17,63 +7,6 @@ export function initSentry() {
     dsn: import.meta.env.VITE_SENTRY_DSN as string,
     environment: "production",
     release: GIT_SHA,
-    integrations: [
-      new Integrations.BrowserTracing({
-        tracingOrigins: [BACKEND_HOST],
-        beforeNavigate: (context) => {
-          const resultEvent = PATTERN_EVENT.exec(context.name);
-          if (resultEvent) {
-            return {
-              ...context,
-              name: "/events/:eventId",
-            };
-          }
-
-          const resultShare = PATTERN_SHARED_ITEM.exec(context.name);
-          if (resultShare) {
-            return {
-              ...context,
-              name: "/s/:shareId",
-            };
-          }
-
-          const resultStravaActivity = PATTERN_STRAVA_ACTIVITY.exec(
-            context.name
-          );
-          if (resultStravaActivity) {
-            return {
-              ...context,
-              name: "/strava-activities/:stravaActivityId",
-            };
-          }
-          const resultCustomRoute = PATTERN_CUSTOM_ROUTE.exec(context.name);
-          if (resultCustomRoute) {
-            return {
-              ...context,
-              name: "/:worldSlug/custom-route",
-            };
-          }
-
-          const resultRoute = PATTERN_ROUTE_OR_SEGMENT.exec(context.name);
-          if (resultRoute) {
-            return {
-              ...context,
-              name: "/:worldSlug/:routeSlug",
-            };
-          }
-
-          const resultWorld = PATTERN_WORLD.exec(context.name);
-          if (resultWorld) {
-            return {
-              ...context,
-              name: "/:worldSlug",
-            };
-          }
-
-          return context;
-        },
-      }),
-    ],
-    tracesSampleRate: 0.01,
+    tracesSampleRate: 0,
   });
 }
