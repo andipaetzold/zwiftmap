5b585236fc42a566153933e2fb2430f4e908df68
Andi Pätzold <github@andipaetzold.com>
2023-01-17 13:09:04 +0100

    fix: do not use zustand default export

--- frontend/src/hooks/useSettings.ts
+++ frontend/src/hooks/useSettings.ts
@@ -1,4 +1,4 @@
-import create from "zustand";
+import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import { Sport } from "zwift-data";
 import { Overlay, Settings, Theme, Units } from "../types";
--- frontend/src/hooks/useStore.ts
+++ frontend/src/hooks/useStore.ts
@@ -1,4 +1,4 @@
-import create from "zustand";
+import { create } from "zustand";
 import { HoverState } from "../types";
 
 interface Store {
