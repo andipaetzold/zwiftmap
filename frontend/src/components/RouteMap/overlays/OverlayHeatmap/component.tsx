9cdc6d8c6ca9e83cfe7a6f9b730bb40902adf9ee
Andi Pätzold <github@andipaetzold.com>
2023-01-19 00:43:47 +0100

    fix: update url

--- frontend/src/components/RouteMap/overlays/OverlayHeatmap/component.tsx
+++ frontend/src/components/RouteMap/overlays/OverlayHeatmap/component.tsx
@@ -9,21 +9,11 @@ export function OverlayHeatmapComponent({ world }: Props) {
   return (
     <LayersControl.Overlay name="Heatmap">
       <LayerGroup>
-        <Lines world={world} />
+        <TileLayer
+          url={`http://localhost:3002/${world.slug}/{z}/{x}/{y}.png`}
+          bounds={world.bounds}
+        />
       </LayerGroup>
     </LayersControl.Overlay>
-  );
-}
-
-interface Props {
-  world: World;
-}
-
-function Lines({ world }: Props) {
-  return (
-    <TileLayer
-      url={`http://localhost:3001/{z}/{x}/{y}.png`}
-      bounds={world.bounds}
-    />
   );
 }
