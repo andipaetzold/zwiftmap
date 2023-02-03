5a2fa450d4860bd4404322bfbf48046f17da5200
Andi Pätzold <github@andipaetzold.com>
2023-02-03 23:49:12 +0100

    fix: move glasgow by new bounds

--- backend/src/shared/browser/roads/roads/scotland/index.ts
+++ backend/src/shared/browser/roads/roads/scotland/index.ts
@@ -54,14 +54,14 @@ createEdge(PADDOCK_WEST_FRONT_2, PADDOCK_WEST_FRONT_3, []);
 createEdge(PADDOCK_WEST_FRONT_3, PADDOCK_WEST_FRONT_4, []);
 
 // Paddock East
-const PADDOCK_EAST_EXIT = createNode([55.640732, -5.218527, 42]);
-const PADDOCK_EAST_FRONT = createNode([55.641344, -5.2185, 42]);
-const PADDOCK_EAST_BACK = createNode([55.642043, -5.218522, 42]);
-const PADDOCK_EAST_CENTER = createNode([55.643175, -5.218511, 42]);
-const PADDOCK_EAST_1 = createNode([55.642088, -5.220158, 42]);
-const PADDOCK_EAST_2 = createNode([55.642082, -5.216982, 42]);
-const PADDOCK_EAST_3 = createNode([55.642754, -5.220072, 42]);
-const PADDOCK_EAST_4 = createNode([55.642748, -5.217025, 42]);
+const PADDOCK_EAST_EXIT = createNode([55.64073, -5.218669, 42]);
+const PADDOCK_EAST_FRONT = createNode([55.641328, -5.218656, 42]);
+const PADDOCK_EAST_BACK = createNode([55.641997, -5.218635, 42]);
+const PADDOCK_EAST_CENTER = createNode([55.643151, -5.218661, 42]);
+const PADDOCK_EAST_1 = createNode([55.642067, -5.220292, 42]);
+const PADDOCK_EAST_2 = createNode([55.642064, -5.217116, 42]);
+const PADDOCK_EAST_3 = createNode([55.642718, -5.220196, 42]);
+const PADDOCK_EAST_4 = createNode([55.642724, -5.21717, 42]);
 createEdge(PADDOCK_EAST_EXIT, PADDOCK_EAST_FRONT, []);
 createEdge(PADDOCK_EAST_FRONT, PADDOCK_EAST_BACK, []);
 createEdge(PADDOCK_EAST_BACK, PADDOCK_EAST_CENTER, []);
@@ -71,7 +71,7 @@ createEdge(PADDOCK_EAST_3, PADDOCK_EAST_BACK, []);
 createEdge(PADDOCK_EAST_4, PADDOCK_EAST_BACK, []);
 
 // Glasgow
-const GLASGOW_INTERSECTION_SOUTH = createNode([55.639215, -5.218528, 41.8]);
+const GLASGOW_INTERSECTION_SOUTH = createNode([55.639193, -5.218657, 41.8]);
 const GLASGOW_EXIT_1 = createNode([55.638207, -5.222368, 0]);
 const GLASGOW_EXIT_2 = createNode([55.638425, -5.223076, 0]);
 const GLASGOW_EXIT_3 = createNode([55.638249, -5.223452, 0]);
